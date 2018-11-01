7c9596c0caa011450858111a581dc75b61b987cd
dustin.liu62 <dustin.liu62@TI03BY45>
2018-11-01 09:55:08 +0800

    ready for pratice

--- LinqTests/YourOwnLinq.cs
+++ LinqTests/YourOwnLinq.cs
@@ -5,18 +5,6 @@ namespace LinqSample.YourOwnLinq
 {
     internal static class YourOwnLinq
     {
-        public static IEnumerable<TSource> YourWhere<TSource>(this IEnumerable<TSource> source,
-            Func<TSource, int, bool> predicate)
-        {
-            var index = 0;
-
-            foreach (var item in source)
-            {
-                if (predicate(item, index))
-                {
-                    yield return item;
-                }
-            }
-        }
+      
     }
 }
