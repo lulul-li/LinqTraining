c8d62e67ba0e1a7343b5be0d811516c098301d9f
dustin.liu62 <dustin.liu62@TI03BY45>
2018-11-08 18:39:00 +0800

    first commit

--- LinqTests/LinqTest.cs
+++ LinqTests/LinqTest.cs
@@ -1,5 +1,7 @@
 ﻿using ExpectedObjects;
+using LinqSample.WithoutLinq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,8 +14,7 @@ namespace LinqTests
         public void find_products_that_price_between_200_and_500()
         {
             var products = RepositoryFactory.GetProducts().ToList();
-            var actual = products.Where(product => product.IsTopSaleProducts());
-
+            var actual = WithoutLinq.Find(products);
             var expected = new List<Product>()
             {
                 new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
@@ -24,12 +25,11 @@ namespace LinqTests
             expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
-        [Ignore]
         [TestMethod]
-        public void find_products_that_price_between_200_and_500_and_cost_more_than_50()
+        public void find_products_that_price_between_200_and_500_and_cost_more_than_30()
         {
             var products = RepositoryFactory.GetProducts();
-
+            var actual = WithoutLinq.Find(products, p => p.Price > 200 && p.Price < 500 && p.Cost > 30);
             var expected = new List<Product>()
             {
                 //new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
@@ -37,15 +37,15 @@ namespace LinqTests
                 new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
             };
 
-            //expected.ToExpectedObject().ShouldEqual(actual.ToList());
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
-        [Ignore]
         [TestMethod]
         public void find_employee_that_age_more_30_item_index_more_2()
         {
             var employees = RepositoryFactory.GetEmployees();
-
+            //var actual = WithoutLinq.Find<Employee>(employees, (e, index) => e.Age > 30 && index >= 2);
+            var actual = employees.Find<Employee>((e, index) => e.Age > 30 && index >= 2);
             var expected = new List<Employee>()
             {
                 //new Employee {Name = "Joe", Role = RoleType.Engineer, MonthSalary = 100, Age = 44, WorkingYear = 2.6},
@@ -63,23 +63,22 @@ namespace LinqTests
             //    Console.WriteLine(item.Price);
             //}
 
-            //expected.ToExpectedObject().ShouldEqual(actual.ToList());
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
-        [Ignore]
         [TestMethod]
         public void YourWhere_YourSelect()
         {
             var employees = RepositoryFactory.GetEmployees();
 
-            //var actual = employees
-            //    .YourWhere(e => e.Age < 25)
-            //    .YourSelect(e => $"{e.Role}:{e.Name}");
+            var actual = employees
+                .YourWhere(e => e.Age < 25)
+                .YourSelect(e => $"{e.Role}:{e.Name}");
 
-            //foreach (var titleName in actual)
-            //{
-            //    Console.WriteLine(titleName);
-            //}
+            foreach (var titleName in actual)
+            {
+                Console.WriteLine(titleName);
+            }
 
             var expected = new List<string>()
             {
@@ -87,55 +86,55 @@ namespace LinqTests
                 "Engineer:Frank",
             };
 
-            //expected.ToExpectedObject().ShouldEqual(actual.ToList());
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
-        [Ignore]
         [TestMethod]
         public void Take()
         {
             var employees = RepositoryFactory.GetEmployees();
+            var actual = WithoutLinq.Take(employees, 2);
             var expected = new List<Employee>
             {
                 new Employee {Name = "Joe", Role = RoleType.Engineer, MonthSalary = 100, Age = 44, WorkingYear = 2.6},
                 new Employee {Name = "Tom", Role = RoleType.Engineer, MonthSalary = 140, Age = 33, WorkingYear = 2.6},
             };
 
-            //expected.ToExpectedObject().ShouldEqual(act.ToList());
+            expected.ToExpectedObject().ShouldEqual(actual.ToList());
         }
 
-        [Ignore]
         [TestMethod]
         public void Skip()
         {
             var employees = RepositoryFactory.GetEmployees();
+            var act = WithoutLinq.YourSkip(employees, 6);
             var expected = new List<Employee>
             {
                 new Employee {Name = "Frank", Role = RoleType.Engineer, MonthSalary = 120, Age = 16, WorkingYear = 2.6},
                 new Employee {Name = "Joey", Role = RoleType.Engineer, MonthSalary = 250, Age = 40, WorkingYear = 2.6},
             };
 
-            //expected.ToExpectedObject().ShouldEqual(act.ToList());
+            expected.ToExpectedObject().ShouldEqual(act.ToList());
         }
 
-        [Ignore]
         [TestMethod]
         public void TakeWhile()
         {
             var employees = RepositoryFactory.GetEmployees();
+            var act = WithoutLinq.YourTakeWhile(employees, 2, e => e.MonthSalary > 150);
             var expected = new List<Employee>
             {
                 new Employee {Name = "Kevin", Role = RoleType.Manager, MonthSalary = 380, Age = 55, WorkingYear = 2.6},
                 new Employee {Name = "Bas", Role = RoleType.Engineer, MonthSalary = 280, Age = 36, WorkingYear = 2.6},
             };
-            //expected.ToExpectedObject().ShouldEqual(act.ToList());
+            expected.ToExpectedObject().ShouldEqual(act.ToList());
         }
 
-        [Ignore]
         [TestMethod]
         public void SkipWhile()
         {
             var employees = RepositoryFactory.GetEmployees();
+            var act = WithoutLinq.YourSkipWhile(employees, 3, e => e.MonthSalary < 150);
             var expected = new List<Employee>
             {
                 new Employee {Name = "Kevin", Role = RoleType.Manager, MonthSalary = 380, Age = 55, WorkingYear = 2.6},
@@ -144,7 +143,7 @@ namespace LinqTests
                 new Employee {Name = "Frank", Role = RoleType.Engineer, MonthSalary = 120, Age = 16, WorkingYear = 2.6},
                 new Employee {Name = "Joey", Role = RoleType.Engineer, MonthSalary = 250, Age = 40, WorkingYear = 2.6},
             };
-            //expected.ToExpectedObject().ShouldEqual(act.ToList());
+            expected.ToExpectedObject().ShouldEqual(act.ToList());
         }
     }
 }
--- LinqTests/WithoutLinq.cs
+++ LinqTests/WithoutLinq.cs
@@ -1,11 +1,130 @@
-﻿using System;
+﻿using LinqTests;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace LinqSample.WithoutLinq
 {
     internal static class WithoutLinq
     {
+        public static IEnumerable<Product> Find(List<Product> products)
+        {
+            foreach (var product in products)
+            {
+                if (product.IsTopSaleProducts())
+                {
+                    yield return product;
+                }
+            }
+        }
 
+        public static IEnumerable<Product> Find(IEnumerable<Product> products, Predicate<Product> p)
+        {
+            foreach (var product in products)
+            {
+                if (p(product))
+                {
+                    yield return product;
+                }
+            }
+        }
+
+        internal static IEnumerable<T> Find<T>(this IEnumerable<T> employees, Func<T, int, bool> p)
+        {
+            var index = 0;
+            foreach (var employee in employees)
+            {
+                if (p(employee, index))
+                {
+                    yield return employee;
+                }
+                index++;
+            }
+        }
+
+        public static IEnumerable<T> YourWhere<T>(this IEnumerable<T> sources, Func<T, bool> p)
+        {
+            foreach (var source in sources)
+            {
+                if (p(source))
+                {
+                    yield return source;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> YourSelect<T, TResult>(this IEnumerable<T> sources, Func<T, TResult> selector)
+        {
+            foreach (var source in sources)
+            {
+                yield return selector(source);
+            }
+        }
+
+        public static IEnumerable<T> Take<T>(IEnumerable<T> sources, int i)
+        {
+            var index = 0;
+            foreach (var source in sources)
+            {
+                if (index < i)
+                {
+                    yield return source;
+                }
+                index++;
+            }
+        }
+
+        public static IEnumerable<T> YourSkip<T>(IEnumerable<T> sources, int i)
+        {
+            var enumerator = sources.GetEnumerator();
+            var index = 0;
+            while (enumerator.MoveNext())
+            {
+                if (index >= i)
+                {
+                    yield return enumerator.Current;
+                }
+                index++;
+            }
+        }
+
+        public static IEnumerable<T> YourTakeWhile<T>(IEnumerable<T> employees, int i, Func<T, bool> func)
+        {
+            if (employees == null)
+            {
+                throw new ArgumentException();
+            }
+            var enumerator = employees.GetEnumerator();
+            var index = 0;
+            while (enumerator.MoveNext())
+            {
+                if (index >= i)
+                {
+                    yield break;
+                }
+                var current = enumerator.Current;
+                if (func(current))
+                {
+                    yield return current;
+                    index++;
+                }
+            }
+        }
+
+        public static IEnumerable<T> YourSkipWhile<T>(IEnumerable<T> sources, int i, Func<T, bool> func)
+        {
+            var enumerator = sources.GetEnumerator();
+            var index = 0;
+            while (enumerator.MoveNext())
+            {
+                if (index < i && func(enumerator.Current))
+                {
+                    index++;
+                }
+                else
+                {
+                    yield return enumerator.Current;
+                }
+            }
+        }
     }
 }
